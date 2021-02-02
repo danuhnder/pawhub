065f426eba6df33e3bbe843ffea1429bfdf2e1c8
Daniel Robertson <danieljrobertson@gmail.com>
2021-02-02 15:46:16 -0500

    begin html and folder structure

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>PawHub</title>
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+
+    <nav>
+      <div class="wrapper">
+        <a href="index.php"><img src="img/logo.png" alt="PawHub Logo"></a>
+        <ul>
+          <li><a href="index.php">Home</a></li>
+          <li><a href="signup.php">Sign Up</a></li>
+          <li><a href="login.php">Log In</a></li>
+        </ul>
+      </div>
+    </nav>
