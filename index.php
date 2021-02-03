4ebde7785a4e2b75d8675aae19753b1cb26b2ff8
Daniel Robertson <danieljrobertson@gmail.com>
2021-02-03 10:44:36 -0500

    import bootstrap and add some basic styling

--- index.php
+++ index.php
@@ -2,19 +2,21 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>PawHub</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     <link rel="stylesheet" href="css/reset.css">
     <link rel="stylesheet" href="css/style.css">
   </head>
   <body>
 
-    <nav>
-      <div class="wrapper">
-        <a href="index.php"><img src="img/logo.png" alt="PawHub Logo"></a>
-        <ul>
-          <li><a href="index.php">Home</a></li>
-          <li><a href="signup.php">Sign Up</a></li>
-          <li><a href="login.php">Log In</a></li>
+    <nav class="navbar navbar-dark bg-dark">
+      <div class="d-flex flex-row">
+        <a class= "navbar-brand" href="index.php"><img src="img/logo.png" alt="PawHub Logo"></a>
+        <ul class="nav navbar-nav d-flex flex-row">
+          <li class="nav nav-item"><a href="index.php">Home</a></li>
+          <li class="nav nav-item"><a href="signup.php">Sign Up</a></li>
+          <li class="nav nav-item"><a href="login.php">Log In</a></li>
         </ul>
       </div>
     </nav>
@@ -22,7 +24,8 @@
     <div class="wrapper">
       <h1>PawHub The Page</h1> 
     </div>
+  <script src="js/script.js">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
   </body>
   </html>
 
-  <script src="js/script.js">
