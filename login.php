4b094a11cc2fd10d804e3019469acef458936d0a
Daniel Robertson <danieljrobertson@gmail.com>
2021-02-08 11:22:47 -0500

    setup db handlers

---
+++ login.php
@@ -0,0 +1,17 @@
+<?php
+  include_once 'header.php'
+  ?>
+  <section class="login-form">
+      <h1>Log In</h1>
+      <div class="login-form-form">
+        <form action="login.inc.php" method="post">
+          <input type="text" name="name" placeholder="Your name, or your owner's email address">
+          <input type="password" name="password" placeholder="A secret word only you know. Don't tell the cat!">
+          <button type="submit" name="submit">Log In</button>
+        </form>
+      </div>
+  </section>
+
+<?php
+  include_once 'footer.php'
+  ?>
--- signup.php
+++ signup.php
@@ -3,13 +3,15 @@
   ?>
   <section class="signup-form">
       <h1>Sign up</h1>
-      <form action="signup.inc.php" method="post">
-        <input type="text" name="name" placeholder="What do the people who bring you food call you?">
-        <input type="text" name="email" placeholder="Email address for the people who bring you food">
-        <input type="password" name="password" placeholder="A secret word only you know. Don't tell the cat!">
-        <input type="password" name="confirmpassword" placeholder="Give us that secret word again just in case.">
-
-      </form>
+      <div class="signup-form-form">
+        <form action="signup.inc.php" method="post">
+          <input type="text" name="name" placeholder="What do the people who bring you food call you?">
+          <input type="text" name="email" placeholder="Email address for the people who bring you food">
+          <input type="password" name="password" placeholder="A secret word only you know. Don't tell the cat!">
+          <input type="password" name="confirmpassword" placeholder="Give us that secret word again just in case.">
+          <button type="submit" name="submit">Sign Up</button>
+        </form>
+      </div>
   </section>
 
 <?php
